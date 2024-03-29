5f86f8d11ab52e16676a7d6b717731647750a5f2
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-03-29 16:00:13 -0700

    Add search function

--- src/app/weather/components/SearchCity.tsx
+++ src/app/weather/components/SearchCity.tsx
@@ -1,8 +1,25 @@
-export default function SearchCity() {
+import { FormEvent, useRef } from "react";
+
+export default function SearchCity({
+  search,
+}: {
+  search: (keyword: string) => void;
+}) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    inputRef.current && search(inputRef.current.value);
+  };
+
   return (
     <section className='p-4 border-t'>
-      <form className='flex items-center justify-center mt-6'>
+      <form
+        className='flex items-center justify-center mt-6'
+        onSubmit={handleSearch}
+      >
         <input
+          ref={inputRef}
           type='text'
           placeholder='Search...'
           className='w-full bg-white shadow p-3 rounded outline-none focus:ring-2 focus:ring-indigo-400'
