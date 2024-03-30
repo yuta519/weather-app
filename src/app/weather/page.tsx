8d81974474860e160f2e4dffb86dceed8e45afb4
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-03-30 14:54:19 -0700

    Show search city result

--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -1,15 +1,26 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import SearchCity from "@/app/weather/components/SearchCity";
+import Cities from "@/app/weather/components/Cities";
 import useCity from "@/hooks/useCity";
+import { City } from "@/types/City";
 
 export default function Page() {
+  const router = useRouter();
   const { searchCity, cities } = useCity();
+
+  const handleClick = (city: City) => {
+    router.push(
+      `/weather/${city.country}/${city.name}?lat=${city.lat}&lon=${city.lon}`
+    );
+  };
 
   return (
     <div className='flex flex-col'>
       <main className='flex-1'>
         <SearchCity search={searchCity} />
+        {!!cities.length && <Cities cities={cities} onClick={handleClick} />}
       </main>
     </div>
   );
