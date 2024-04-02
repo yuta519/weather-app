e61672c860cd06eb9d37fc40850a53ccb8f72e95
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-04-02 16:42:30 -0700

    Change link text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Link href='/weather'>Weather</Link>
+      <Link href='/weather'>Search City</Link>
     </main>
   );
 }
